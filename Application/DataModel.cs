0bfedc9fc9e154daf72ee963bb139de060f0528d
Dhiraj <D@D>
2017-12-20 07:44:46 +0530

    implemented in dataModel

--- Application/DataModel.cs
+++ Application/DataModel.cs
@@ -13,28 +13,28 @@ namespace ApplicationNamespace
     {
         public void WriteXMLFile(string fullFilePath, int count, FrequencyEnum frequency)
         {
-            //XmlSerializer xml = new XmlSerializer(typeof(List<string>), new Type[] { typeof(string) });
-            //
             RandomDataGenerator generator = new RandomDataGenerator();
-            ////StringBuilder builder = new StringBuilder();
-            using (StreamWriter file = new StreamWriter(fullFilePath))
-            using (XMLFileReaderWriter rw = new XMLFileReaderWriter(file))
-            {
-                {
 
+            using (StreamWriter file = new StreamWriter(fullFilePath))
+            {
+                using (XMLFileReaderWriter rw = new XMLFileReaderWriter(file))
+                {
                     foreach (var item in generator.YieldRandomData(count, frequency))
                         rw.Write(item);
-                    //    {
-                    //        xml.Serialize(file, generator.YieldRandomString(count, frequency).ToList());// builder.Append(item.TickValue)
-                    //    }
                 }
             }
+        }
+
+        public IEnumerable<Data> ReadXMLFile(string fullFilePath, int count, FrequencyEnum frequency)
+        {
             using (StreamReader reader = new StreamReader(fullFilePath))
             {
-                XMLFileReaderWriter rw = new XMLFileReaderWriter(reader);
-                foreach (var item in rw.Read())
+                using (XMLFileReaderWriter rw = new XMLFileReaderWriter(reader))
                 {
-
+                    foreach (var item in rw.Read())
+                    {
+                        yield return item;
+                    }
                 }
             }
         }
